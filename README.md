b51b573c44ec5b57a2d8554e6b573ac86540d8c8
EcomputerApps <134622652+EcomputerApps@users.noreply.github.com>
2024-02-27 18:52:39 +0100

    aa

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ip
+
+A new Flutter project.
---
+++ lib/business_logic/services/network.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:ip/business_logic/model/channel.dart';
+
+Future<List<ChannelObj>?> fetchChannels() async {
+  List<ChannelObj> channels = [];
+
+  try {
+    Response response =
+        await Dio().get('https://iptv-org.github.io/api/channels.json');
+    print("enzo");
+    print(response.toString());
+    for (var channel in response.data) {
+      ChannelObj channelObj = ChannelObj.fromJson(channel);
+
+      if (channelObj.categories.isNotEmpty) {
+        if (channelObj.categories[0].name != "XXX") {
+          channels.add(channelObj);
+        }
+      }
+    }
+    print("salmm");
+
+    print(channels[0].countries[0].name);
+    List<ChannelObj> repairChannels = channels.toSet().toList();
+
+    return repairChannels;
+  } catch (e) {
+    print(e);
+  }
+  return null;
+}
